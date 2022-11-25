cbb904d4e0f783fefc2dd8ff69df649f989d57b4
Jackson Horton <jacksonhorton12@icloud.com>
2022-11-25 15:55:40 -0600

    Redid the brightsign class again. Much cleaner and less repetition

--- brightsign.py
+++ brightsign.py
@@ -1,107 +1,101 @@
 import requests
 import json
+from datetime import datetime as dt
+
 
 class brightsign:
-    _versions = {
-        '8.5.31': 'https://bsncloud.s3.amazonaws.com/public/malibu-8.5.31-update.bsfw'
-    }
     
+    latest_ver = '8.5.31'
     
     def __init__(self, player_ip: str):
         self.player_ip = player_ip
+        self.req_url = f'http://{self.player_ip}/api/v1'
+        self.player_family = None
+        self.version = None
+        self.status = None
+        self._getPlayerInfo()
+        self._logInfo()
+      
+      
+    def _logInfo(self) -> None:
+        self._log(f'Info: (family: {self.player_family}, status: {self.status}, version: {self.version})')
     
-    def update(self, version: str = '8.5.31'):
+    def _log(self, msg: str) -> None:
         
-        # check that passed version is in the versions dict
-        try:
-            self._versions[version]
-        except KeyError:
-            print(f'Invalid version passed! {version} is not in list of versions.')
-            return
+        with open('log', 'a') as file:
+            time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+            file.write(f'{time}> {(self.player_ip)} {msg}\n')
+
         
-        
-        # req_url is where api requests are made to the brightsign
-        req_url = f'http://{self.player_ip}/api/v1'
-        
-        
-        # check status of player
+    def _getPlayerInfo(self) -> None:
+        # checks status, version, and family of brightsign
         try:
             # send health request to player and try to parse json for result
-            res = requests.get(f'{req_url}/health')
-            json_res = json.loads(res.text)
-            status = json_res['data']['result']['status']
-            
-            # Log health
-            with open('log', 'a') as file:
-                file.write(f'{self.player_ip} Health: {status}\n')
-            
+            health_res = requests.get(f'{self.req_url}/health')
+            json_res = json.loads(health_res.text)
+            self.status = json_res['data']['result']['status']
+
         except KeyError as exc:
             # Log error
-            with open('log', 'a') as file:
-                file.write(f'(ERROR) {self.player_ip} : {res.status_code}\n')
-                file.write(f'(ERROR) {exc} : Couldn\'t find version in json\n')
-            print(f'KeyError: {exc}. Couldn\'t get the current version.')
+            self._log(f'Error: {health_res.status_code}')
+            self._log(f'Error: {exc}; Couldn\'t find health in json')
+            self._log(json_res)
+            return
+        except requests.exceptions.ConnectionError as exc:
+            self._log(f'Error connecting to player at {self.player_ip}: {exc}')
+            return
+        
+        try:
+            # send version request to player and try to parse json for result
+            health_res = requests.get(f'{self.req_url}/info')
+            json_res = json.loads(health_res.text)
+            self.player_family = json_res['data']['result']['family']
+            self.version = json_res['data']['result']['FWVersion']
+
+
+        except KeyError as exc:
+            # Log error
+            self._log(f'Error: {health_res.status_code}')
+            self._log(f'Error: {exc}; Couldn\'t find a key in json')
+            self._log(json_res)
+            return
+    
+    
+    
+    
+    def update(self, version: str = latest_ver):
+        # check health before trying to do anything
+        if self.status != 'active':
+            self._log(f'Not attempting to update, status is not active ({self.status})')
+            print(
+                f'{self.player_ip}: Not attempting to update, status is not active ({self.status})')
             return
         
         # make sure the player isn't already up to date
-        if version == self._check_version():
+        if version == self.version:
             print(f'{self.player_ip} already up to date')
             # Log UTD
-            with open('log', 'a') as file:
-                file.write(f'{self.player_ip} : Already up to date, aborting.\n')
+            self._log(f'Player already up-to-data, version {self.version}')
             return
 
 
         # the endpoint to update firmware
-        update_endpoint = f'/download-firmware?url={self._versions[version]}'
+        update_endpoint = f'/download-firmware?url=https://bsncloud.s3.amazonaws.com/public/{self.player_family}-{self.latest_ver}-update.bsfw'
 
         print(f'Starting on {self.player_ip}')
         try:
             # Send request
-            res = requests.get(f'{req_url}{update_endpoint}')
+            res = requests.get(f'{self.req_url}{update_endpoint}')
             # Try to log
-            with open('log', 'a') as file:
-                file.write(f'{self.player_ip} : {res.status_code}\n')
-                file.write(f'{res.text}\n')
+            self._log(f'Request status ({res.status_code}) : {res.text}')
+            res.close()
             
-            print(f'Finished on {self.player_ip}, status {res.status_code}')
+            print(f'{self.player_ip} completed.')
         except Exception as exc:
             print("Exception:", exc)
             # Log exception that occurred
-            with open('log', 'a') as file:
-                file.write(f'(ERROR) {self.player_ip}: {exc}\n')
+            self._log(f'Error: {exc}')
             return
 
 
-    def _check_version(self) -> str:
-        req_url = f'http://{self.player_ip}/api/v1'
-        # gets the current version of the brightsign
-        try:
-            # send info request
-            res = requests.get(f'{req_url}/info')
-            json_res = json.loads(res.text)
-            version = json_res['data']['result']['FWVersion']
-
-            # Log current version
-            with open('log', 'a') as file:
-                file.write(f'{self.player_ip} Current Version: {version}\n')
-            
-            return version
-
-        except KeyError as exc:
-            # Log error
-            with open('log', 'a') as file:
-                file.write(f'(ERROR) {self.player_ip} : {res.status_code}\n')
-                file.write(
-                    f'(ERROR) {exc} : Couldn\'t find status in json, player assumed not healthy\n')
-            print(
-                f'KeyError: {exc}. Player assumed not healthy, not attempting to update.')
-            return
-        except Exception as exc:
-            # Log error
-            with open('log', 'a') as file:
-                file.write(f'(ERROR) {self.player_ip} : {res.status_code}\n')
-                file.write(
-                    f'(ERROR) {exc}: unexpected exception, not attempting to connect.\n')
-            print(f'{exc} : Player assumed not healthy, not attempting to update.')
-            return
+    
