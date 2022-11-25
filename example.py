aed2b9f2169407400a333931de00f711ca4aa0f5
Jackson Horton <jacksonhorton12@icloud.com>
2022-11-25 16:14:53 -0600

    Added an example file to use the brightsign class.

---
+++ example.py
@@ -0,0 +1,7 @@
+# import the brightsign class
+from brightsign import brightsign
+
+# create a bs object with an ip and then call the update function on it
+# by default, .update() will update to the version specified as latest_ver in the brightsign class
+# this can be overriden by using update with a version parameter: .update('8.5.31') 
+brightsign('10.11.0.1').update()
