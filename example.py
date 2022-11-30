293a32d867c8986f5afcfc656cb2aa44f0fe8ac9
Jackson Horton <jacksonhorton1212@gmail.com>
2022-11-29 20:38:11 -0600

    Added a toggle for the logging system as a constant.python

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ from brightsign import brightsign
 # create a bs object with an ip and then call the update function on it
 # by default, .update() will update to the version specified as latest_ver in the brightsign class
 # this can be overriden by using update with a version parameter: .update('8.5.31') 
-brightsign('10.11.0.1').update()
+brightsign('192.168.1.122').update()
